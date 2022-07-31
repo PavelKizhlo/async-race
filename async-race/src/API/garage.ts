a8e2c11408b061159eb15b8608d13a0c0610291e
Pavel Kizhlo <pavelkizh@gmail.com>
2022-07-31 13:21:36 +0300

    feat: create class "Garage" to interact with api

---
+++ async-race/src/API/garage.ts
@@ -0,0 +1,53 @@
+import { GarageAPI, Car, CarParams } from '../interfaces/interfaces';
+
+class Garage implements GarageAPI {
+    private baseURL: string;
+
+    private garage: string;
+
+    constructor() {
+        this.baseURL = 'http://127.0.0.1:3000';
+        this.garage = `${this.baseURL}/garage`;
+    }
+
+    async getCarsData(page: number, limit: number) {
+        const response = await fetch(`${this.garage}?_page=${page}&_limit=${limit}`);
+
+        return {
+            cars: (await response.json()) as Array<Car>,
+            total: +(response.headers.get('X-Total-Count') as string),
+        };
+    }
+
+    async getCar(id: number) {
+        const response = await fetch(`${this.garage}/${id}`);
+        return response.json();
+    }
+
+    async createCar(params: CarParams) {
+        const response = await fetch(this.garage, {
+            method: 'POST',
+            body: JSON.stringify(params),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+        return (await response.json()) as Car;
+    }
+
+    async deleteCar(id: number) {
+        await fetch(`${this.garage}/${id}`, { method: 'DELETE' });
+    }
+
+    async updateCar(id: number, params: CarParams) {
+        await fetch(`${this.garage}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+    }
+}
+
+export default Garage;
