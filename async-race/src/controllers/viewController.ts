ede8a783e721b12e364b8b4cd2ca30b485c371dd
Pavel Kizhlo <pavelkizh@gmail.com>
2022-08-01 14:07:09 +0300

    feat: implement default view

--- async-race/src/controllers/viewController.ts
+++ async-race/src/controllers/viewController.ts
@@ -1,14 +1,28 @@
+import state from '../app/appData/state';
 import GarageView from '../view/garage/garageView';
+import GarageController from './garageController';
 
 class ViewController {
     private garageView: GarageView;
 
+    private garageController: GarageController;
+
     constructor() {
         this.garageView = new GarageView();
+        this.garageController = new GarageController();
     }
 
-    start() {
-        this.garageView.render();
+    async start() {
+        switch (state.view) {
+            case 'winners':
+                // this.winnersView.render();
+                break;
+            case 'garage':
+            default:
+                this.garageView.render();
+                await this.garageController.start();
+                break;
+        }
     }
 }
 
