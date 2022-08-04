20ec129d6143888feefb3b8c045e1bf3a0009752
Pavel Kizhlo <pavelkizh@gmail.com>
2022-08-04 14:30:29 +0300

    refactor: take out selecting car into separate method

--- async-race/src/controllers/garageController.ts
+++ async-race/src/controllers/garageController.ts
@@ -21,6 +21,7 @@ class GarageController {
     async start() {
         const view = document.getElementById('garage-view') as HTMLDivElement;
         await this.loadPage();
+        this.selectCar();
 
         view.addEventListener('click', async (evt) => {
             const elementID = (evt.target as HTMLElement).id;
@@ -32,14 +33,8 @@ class GarageController {
                 case elementID.includes('remove-button'):
                     await this.removeCar(parseInt((elementID.match(/[0-9]+$/) as RegExpMatchArray)[0], 10));
                     break;
-                case elementID.includes('select-button'):
-                    if ((evt.target as HTMLElement).classList.contains('button_active')) {
-                        (evt.target as HTMLElement).classList.remove('button_active');
-                    } else {
-                        document.querySelector('.button_active')?.classList.remove('button_active');
-                        (evt.target as HTMLElement).classList.add('button_active');
-                    }
-                    this.changeCar();
+                case elementID === 'update-button':
+                    await this.changeCar();
                     break;
                 case elementID === 'generate-button':
                     await this.createRandomCars();
@@ -87,27 +82,43 @@ class GarageController {
         await this.loadPage();
     }
 
-    changeCar() {
+    selectCar() {
+        const view = document.getElementById('garage-view') as HTMLDivElement;
+        const carNameInput = document.getElementById('update-name-input') as HTMLInputElement;
+
+        view.addEventListener('click', (evt) => {
+            const target = evt.target as HTMLElement;
+            if (target.id.includes('select-button')) {
+                if (target.classList.contains('button_active')) {
+                    target.classList.remove('button_active');
+                    carNameInput.value = '';
+                } else {
+                    document.querySelector('.button_active')?.classList.remove('button_active');
+                    target.classList.add('button_active');
+                    const id = parseInt((target.id.match(/[0-9]+$/) as RegExpMatchArray)[0], 10);
+                    const prevName = (document.getElementById(`car-name-${id}`) as HTMLSpanElement).innerHTML;
+                    carNameInput.value = prevName;
+                }
+            }
+        });
+    }
+
+    async changeCar() {
         const carNameInput = document.getElementById('update-name-input') as HTMLInputElement;
         const carColorInput = document.getElementById('update-color-input') as HTMLInputElement;
-        const updateButton = document.getElementById('update-button') as HTMLButtonElement;
+        const selectedCar = document.querySelector('.button_active') as HTMLButtonElement;
 
-        updateButton.addEventListener('click', async () => {
-            const selectedCar = document.querySelector('.button_active') as HTMLButtonElement;
+        if (selectedCar) {
+            const id = parseInt((selectedCar.id.match(/[0-9]+$/) as RegExpMatchArray)[0], 10);
+            const prevName = (document.getElementById(`car-name-${id}`) as HTMLSpanElement).innerHTML;
+            await this.garage.updateCar(id, {
+                name: carNameInput.value ? carNameInput.value : prevName,
+                color: carColorInput.value,
+            });
+            carNameInput.value = '';
+        }
 
-            if (selectedCar) {
-                const id = parseInt((selectedCar.id.match(/[0-9]+$/) as RegExpMatchArray)[0], 10);
-                const prevName = (document.getElementById(`car-name-${id}`) as HTMLSpanElement).innerHTML;
-                await this.garage.updateCar(id, {
-                    name: carNameInput.value ? carNameInput.value : prevName,
-                    color: carColorInput.value,
-                });
-                carNameInput.value = '';
-            }
-
-            const carsData = await this.garage.getCarsData(state.garagePage);
-            this.garagePage.render(state.garagePage, carsData.cars);
-        });
+        await this.loadPage();
     }
 
     async createRandomCars() {
