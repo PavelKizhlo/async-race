37531278a1383d679960d74d7352932b3082916d
Pavel Kizhlo <pavelkizh@gmail.com>
2022-08-07 14:56:44 +0300

    feat: add titlle with custom font

--- async-race/src/view/commonView.ts
+++ async-race/src/view/commonView.ts
@@ -5,6 +5,7 @@ class CommonView implements View {
         const container = document.getElementById('app') as HTMLBodyElement;
 
         container.innerHTML = `
+          <h1 class="title">Async Race</h1>
           <div class="nav">
             <button class="nav__button button" id="to-garage">to garage</button>
             <button class="nav__button button" id="to-winners">to winners</button>
